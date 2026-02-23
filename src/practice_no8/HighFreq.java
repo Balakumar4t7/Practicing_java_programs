75a99adeff2bd8c94d32d02cf2af23412794cc81
Balakumar <balakumarbkt2005@gmail.com>
2026-02-23 22:28:54 +0530

    Updated program for Finding the Highest Occurence of a char in a string

--- src/practice_no8/HighFreq.java
+++ src/practice_no8/HighFreq.java
@@ -3,9 +3,17 @@ package practice_no8;
 public class HighFreq {
     public static void main(String[] args){
         String s ="Engineering";
-        char arr[] = new char[100];
+        s=s.toLowerCase();
+        int arr[] = new int[26];
+        int max=0;
         for(int i=0 ; i<s.length();i++){
-
+            arr[s.charAt(i)-'a']=arr[s.charAt(i)-'a']+1;
+            if(max<arr[s.charAt(i)-'a']) max=arr[s.charAt(i)-'a'];
+        }
+        System.out.println("maximum number of occurence "+ max);
+        for(int i=0;i<arr.length;i++) {
+            char ch= (char)(i+'a');
+            if(arr[i]==max) System.out.println(ch + " " +arr[i] );
         }
     }
 }
